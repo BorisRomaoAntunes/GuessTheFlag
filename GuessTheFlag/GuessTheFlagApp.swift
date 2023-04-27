908ecc0b41df20bf974a64d46861753cdea36e1a
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-04-27 18:39:51 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Boris R on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
